92238e87b9115bd46fe1170259d946463f8da912
mespana81569 <alejoespana2000@gmail.com>
2026-05-09 20:20:28 -0500

    :)

--- firmware/secrets.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#define HOST               "68.183.26.216"
-#define DB_VERIFY_PORT     5000
-#define MQTT_USERNAME      "espectrografo"
-#define MQTT_PASSWORD      "Un1Camacho_spectrografo$2026"
-#define FLASK_API_KEY      "3789e3904273e9e7705a13f667e3e688fd5cacb6082d6e19ab40eb030e54f24f"
